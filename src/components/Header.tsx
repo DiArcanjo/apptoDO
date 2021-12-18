300ba58c23899df35848bd8afb1e1713ea15dc09
Diana Arcanjo <arcanjocode@gmail.com>
2021-12-18 16:03:30 -0300

    Finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,26 @@ interface HeaderProps {
 
 export function Header({ tasksCounter }: HeaderProps) {
   // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
-  
+
+  function implementTaskCounter(tarefas: Number) {
+    let countTarefas
+    if (tarefas === 1) {
+      return countTarefas = 'tarefa'
+    } else {
+      return countTarefas = 'tarefas'
+    }
+
+  }
+
+  const tasksCounterText = implementTaskCounter(tasksCounter)
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} />
-      
+
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        {<Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>}
       </View>
     </View>
   )
